3846aaabc563d7dd6f0477e22afc45be11e8136e
BandiSupriya1 <136787462+BandiSupriya1@users.noreply.github.com>
2025-03-21 19:18:24 +0530

    Update CucumberTestRunner.java

--- src/test/java/com/ecommerce/runners/CucumberTestRunner.java
+++ src/test/java/com/ecommerce/runners/CucumberTestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/Checkout.feature",
+        features = "src/test/resources/features",
         glue = "com.ecommerce.stepDefinitions",
       // tags="@CheckoutFunctionsSignup",
      //   plugin = {"pretty", "html:target/cucumber-reports.html"},
